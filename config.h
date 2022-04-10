7d94d7bf57f74c2a831172aadc249f5256f60fa0
FlyingBBQ <derek.cardol@gmail.com>
2022-04-10 18:07:58 +0200

    Add rule for Signal

--- config.h
+++ config.h
@@ -42,6 +42,7 @@ static const Rule rules[] = {
 	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
     { "discord",  NULL,       NULL,       1 << 1,       0,           1 },
     { "Spotify",  NULL,       NULL,       1,            0,           1 },
+    { "Signal",   NULL,       NULL,       1 << 2,       0,           1 },
 };
 
 /* layout(s) */
